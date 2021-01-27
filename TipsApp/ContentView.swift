70ec067ddacf15ac5336212a4fab39212b29acd9
Eric Baker <elb5465@gmail.com>
2021-01-27 17:00:26 -0500

    Initial Commit

---
+++ TipsApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TipsApp
+//
+//  Created by Eric Baker on 1/27/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TipsApp/TipsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipsAppApp.swift
+//  TipsApp
+//
+//  Created by Eric Baker on 1/27/21.
+//
+
+import SwiftUI
+
+@main
+struct TipsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
